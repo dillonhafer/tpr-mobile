7feb058c3f8c14199908fab5a6f8b80839aacd79
Dillon Hafer <dh@dillonhafer.com>
2019-03-31 17:45:38 -0400

    Update build task

--- tasks/release.js
+++ tasks/release.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
-const { execSync, exec } = require('child_process');
-const { task, info, abort } = require('./utils');
-const Upload = require('./upload');
+const fs = require("fs");
+const { execSync, exec } = require("child_process");
+const { task, info, abort } = require("./utils");
+const Upload = require("./upload");
 
 const checkCleanGit = () => {
-  task('Checking for clean git status');
+  task("Checking for clean git status");
   try {
-    execSync('git diff-index --quiet HEAD --');
-    info('Git repo is clean');
+    execSync("git diff-index --quiet HEAD --");
+    info("Git repo is clean");
   } catch (err) {
-    abort('Dirty git repo', err);
+    abort("Dirty git repo", err);
   }
 };
 
 let newBuildNumber;
 const bumpVersion = () => {
-  task('Bumping version');
+  task("Bumping version");
   // Load file
-  let app = JSON.parse(fs.readFileSync('app.json', 'utf8'));
+  let app = JSON.parse(fs.readFileSync("app.json", "utf8"));
 
   // Compute new version
   newBuildNumber = app.expo.android.versionCode + 1;
@@ -29,18 +29,18 @@ const bumpVersion = () => {
 
   // Write file
   app = JSON.stringify(app, null, 2);
-  fs.writeFileSync('app.json', app, 'utf8');
+  fs.writeFileSync("app.json", app, "utf8");
 };
 
 const commitGitVersion = build => {
-  task('Committing new version');
+  task("Committing new version");
   const add = `git add -A`;
   const commit = `git commit -m 'Bump version to ${build}'`;
   try {
     execSync(add);
     execSync(commit);
   } catch (err) {
-    abort('Failed to commit new version');
+    abort("Failed to commit new version");
   }
 };
 
@@ -49,7 +49,7 @@ const expoBuild = (platform, runAsync = false) => {
   try {
     const execFunction = runAsync ? exec : execSync;
     const buildOutput = execFunction(
-      `expo-cli build:${platform} --release-channel production`,
+      `expo-cli build:${platform} --release-channel production`
     ).toString();
     const url = buildOutput.match(/(https:\/\/expo\.io\/build.*)/)[0];
     info(`Open status URL: ${url}`);
@@ -60,28 +60,28 @@ const expoBuild = (platform, runAsync = false) => {
 };
 
 const pushGitRemote = () => {
-  task('Push to github');
+  task("Push to github");
   try {
-    execSync('git push');
+    execSync("git push");
   } catch (err) {
-    abort('Could not push to remote');
+    abort("Could not push to remote");
   }
 };
 
 checkCleanGit();
 bumpVersion();
 
-if (expoBuild('ios')) {
+if (expoBuild("ios")) {
   commitGitVersion(newBuildNumber);
   pushGitRemote();
 
   // Build android in background
-  expoBuild('android', true);
+  // expoBuild('android', true);
 
   // Download IPA/Upload to iTunes
-  Upload.createTmpDir();
-  Upload.getDownloadUrl();
-  Upload.download();
-  Upload.validate();
-  Upload.uploadTask();
+  // Upload.createTmpDir();
+  // Upload.getDownloadUrl();
+  // Upload.download();
+  // Upload.validate();
+  // Upload.uploadTask();
 }
